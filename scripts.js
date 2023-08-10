80350c8e0f1474e5be45e4dacd0381ffcb1a577e
lyrias <java.lyria@gmail.com>
2023-08-10 11:20:13 +0200

    2 commit

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 const video = document.createElement('video');
 const canvas = document.getElementById('cameraCanvas');
 const context = canvas.getContext('2d');
+let currentStream;
 
-let selectedCamera = 'environment'; // Valor por defecto para la cámara trasera
-
-// Acceder a la cámara con la opción de cámara seleccionada
+// Función para acceder a la cámara
 const startCamera = async (facingMode) => {
+    if (currentStream) {
+        currentStream.getTracks().forEach(track => track.stop());
+    }
+    
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: facingMode } });
+        currentStream = stream;
         video.srcObject = stream;
         video.play();
     } catch (error) {
@@ -17,13 +21,12 @@ const startCamera = async (facingMode) => {
 
 // Cambiar entre cámaras frontal y trasera
 const toggleCamera = () => {
-    selectedCamera = (selectedCamera === 'user') ? 'environment' : 'user';
-    video.srcObject.getTracks().forEach(track => track.stop());
-    startCamera(selectedCamera);
+    const facingMode = (currentStream && currentStream.getVideoTracks()[0].getSettings().facingMode === 'user') ? 'environment' : 'user';
+    startCamera(facingMode);
 };
 
-// Iniciar con la cámara seleccionada
-startCamera(selectedCamera);
+// Iniciar con la cámara frontal
+startCamera('user');
 
 // Actualizar el canvas con los frames de la cámara
 video.addEventListener('play', () => {
